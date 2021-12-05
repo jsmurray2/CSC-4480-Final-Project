38a754997fc1318ff6993d758bc8808208666ea6
Jack Murray <jsmurray2@gmail.com>
2021-12-05 17:56:08 -0500

    Created SQL file for data population

---
+++ SQL/movie-data.sql
@@ -0,0 +1,22 @@
+-- Generates data to populate the movie database
+
+-- Movies
+--------------------------------------------------
+
+insert into movie values
+(1, "Star Wars: Episode IV - A New Hope", "Sci-Fi", 10, "1977");
+
+insert into movie values
+(2, "Star Wars: Episode V - The Empire Strikes Back", "Sci-Fi", 10, "1980");
+
+insert into movie values
+(3, "Star Wars: Episode VI - The Return of the Jedi", "Sci-Fi", 9, "1983");
+
+insert into movie values
+(4, "Indiana Jones and the Raiders of the Lost Ark", "Action", 10, "1981");
+
+insert into movie values
+(5, "Indiana Jones and the Temple of Doom", "Action", 8, "1984");
+
+insert into movie values
+(6, "Indiana Jones and the Last Crusade", "Action", 9, "1989");
