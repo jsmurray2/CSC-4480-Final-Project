e4e1a165449c7424770447782f769a68c1130552
noahtkramer <noahtkramer@gmail.com>
2021-12-05 23:43:57 -0500

    Added some actors

--- SQL/movie-data.sql
+++ SQL/movie-data.sql
@@ -48,6 +48,67 @@ INSERT INTO MOVIE VALUES
  
 -- Actors
 --------------------------------------------------
+INSERT INTO ACTOR VALUES
+(1, 'Brad Pitt', '1963-12-18');
+
+INSERT INTO ACTOR VALUES
+(2, 'Christoph Waltz', '1956-10-04');
+
+INSERT INTO ACTOR VALUES
+(3, 'Harvey Keitel', '1939-05-13');
+
+INSERT INTO ACTOR VALUES
+(4, 'Tim Roth', '1961-05-14');
+
+INSERT INTO ACTOR VALUES
+(5, 'John Travolta', '1954-02-18');
+
+INSERT INTO ACTOR VALUES
+(6, 'Bruce Willis', '1955-03-19');
+
+INSERT INTO ACTOR VALUES
+(7, 'Jamie Foxx', '1967-12-13');
+
+INSERT INTO ACTOR VALUES
+(8, 'Leonardo DiCaprio', '1974-11-11');
+
+INSERT INTO ACTOR VALUES
+(9, 'Samuel L. Jackson', '1948-12-21');
+
+INSERT INTO ACTOR VALUES
+(10, 'R. Lee Ermey', '1944-03-24');
+
+INSERT INTO ACTOR VALUES
+(11, 'Vincent D''Onofrio', '1959-06-30');
+
+INSERT INTO ACTOR VALUES
+(12, 'Matthew Modine', '1959-03-22');
+
+INSERT INTO ACTOR VALUES
+(13, 'Christian Bale', '1974-01-30');
+
+INSERT INTO ACTOR VALUES
+(14, 'Ethan Hawke', '1970-11-06');
+
+INSERT INTO ACTOR VALUES
+(15, 'Illeana Douglas', '1965-07-25');
+
+INSERT INTO ACTOR VALUES
+(16, 'Jack Nicholson', '1937-04-22');
+
+INSERT INTO ACTOR VALUES
+(17, 'Shelley Duvall', '1949-07-07');
+
+INSERT INTO ACTOR VALUES
+(18, 'Ben Stiller', '1965-11-30');
+
+INSERT INTO ACTOR VALUES
+(19, 'Robert De Niro', '1943-08-17');
+
+INSERT INTO ACTOR VALUES
+(20, 'Keir Dullea', '1936-05-30');
+
+
 
 -- Stars In
 --------------------------------------------------
