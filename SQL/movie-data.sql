97fe8684415eb53eff997c899475eb5983b0e96c
noahtkramer <noahtkramer@gmail.com>
2021-12-05 23:21:24 -0500

    Added more movies

---
+++ SQL/movie-data.sql
@@ -0,0 +1,65 @@
+insert into movie values
+(1, 'Star Wars: Episode IV - A New Hope', 'Sci-Fi', 10, '1977');
+
+insert into movie values
+(2, 'Star Wars: Episode V - The Empire Strikes Back', 'Sci-Fi', 10, '1980');
+
+insert into movie values
+(3, 'Star Wars: Episode VI - The Return of the Jedi', 'Sci-Fi', 9, '1983');
+
+insert into movie values
+(4, 'Indiana Jones and the Raiders of the Lost Ark', 'Action', 10, '1981');
+
+insert into movie values
+(5, 'Indiana Jones and the Temple of Doom', 'Action', 8, '1984');
+
+insert into movie values
+(6, 'Indiana Jones and the Last Crusade', 'Action', 9, '1989');
+
+INSERT INTO MOVIE VALUES 
+ (7, 'Inglourious Basterds', 'Adventure', 8.3, '2009');
+ 
+ INSERT INTO MOVIE VALUES 
+ (8, 'Resevoir Dogs', 'Crime', 8.3, '1992');
+ 
+ INSERT INTO MOVIE VALUES 
+ (9, 'Pulp Fiction', 'Crime', 8.9, '1994');
+ 
+ INSERT INTO MOVIE VALUES 
+ (10, 'Django Unchained', 'Drama', 8.4, '2012');
+ 
+  INSERT INTO MOVIE VALUES 
+ (11, 'Full Metal Jacket', 'War', 8.3, '1987');
+ 
+ INSERT INTO MOVIE VALUES 
+ (12, 'American Psycho', 'Drama', 7.6, '2000');
+ 
+ INSERT INTO MOVIE VALUES 
+ (13, 'Alive', 'Drama', 7.1, '1993');
+ 
+  INSERT INTO MOVIE VALUES 
+ (14, 'The Shining', 'Horror', 8.4, '1980');
+ 
+  INSERT INTO MOVIE VALUES 
+ (15, 'Meet The Parents', 'Comedy', 7, '2000');
+ 
+  INSERT INTO MOVIE VALUES 
+ (16, '2001: A Space Odyssey', 'Sci-Fi', 8.3, '1968');
+ 
+-- Actors
+--------------------------------------------------
+
+-- Stars In
+--------------------------------------------------
+
+-- Director
+--------------------------------------------------
+
+-- Directs
+--------------------------------------------------
+
+-- Production Studio
+--------------------------------------------------
+
+-- Produces
+-------------------------------------------------- 
